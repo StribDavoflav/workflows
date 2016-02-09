49ff52ad12b92cb315820fd3ad653e2ea1813968
Braunger, David <David.Braunger@startribune.com>
2016-02-09 14:56:11 -0600

    Working at getting project template in place

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ jsonSources,
 outputDir,
 sassStyle;
 
-env = process.env.NODE_ENV || 'production';
+env = process.env.NODE_ENV || 'development';
 
 if (env==='development'){
 	outputDir = 'builds/development/';
